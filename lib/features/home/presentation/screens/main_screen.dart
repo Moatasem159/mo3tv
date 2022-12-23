273ccf6e61dfbf615639982f3e6b3265058452d1
moatasem <moatasemmohamed39@gmail.com>
2022-12-23 14:27:16 +0200

    modify main screen

--- lib/features/home/presentation/screens/main_screen.dart
+++ lib/features/home/presentation/screens/main_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mo3tv/features/home/presentation/cubit/bottom_navigation_bar_cubit/bottom_navigation_bar_cubit.dart';
-import 'package:mo3tv/features/home/presentation/widgets/activescreen.dart';
+import 'package:mo3tv/features/home/presentation/cubit/bottom_navigation_bar_cubit/bottom_navigation_bar_states.dart';
 import 'package:mo3tv/features/home/presentation/widgets/bottom_nav_bar.dart';
-
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
@@ -11,12 +10,29 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => BottomNavCubit(),
-      child:  SafeArea(
-        child: Scaffold(
-          backgroundColor: Theme.of(context).backgroundColor,
-          body: const ActiveScreen(),
-          bottomNavigationBar: const BottomNavBar(),
-        ),
+      child: BlocConsumer<BottomNavCubit, BottomNavStates>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          BottomNavCubit cubit=BlocProvider.of<BottomNavCubit>(context);
+          return WillPopScope(
+            onWillPop: () async {
+              if (cubit.index != 0) {
+                cubit.changeIndex(0, context);
+                return false;
+              }
+              else {
+                return true;
+              }
+            },
+            child: SafeArea(
+              child: Scaffold(
+                backgroundColor: Theme.of(context).backgroundColor,
+                body: cubit.screens[cubit.index],
+                bottomNavigationBar: const BottomNavBar(),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
