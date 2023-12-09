127dc7e61d2a1dee55b65301546a2856ce1eeee8
moatasem <moatasemmohamed39@gmail.com>
2023-12-09 17:53:19 +0200

    [MODIFICATION]

--- lib/features/settings/presentation/screens/settings_screen.dart
+++ lib/features/settings/presentation/screens/settings_screen.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:mo3tv/features/settings/presentation/widgets/settings_screen_widgets/settings_screen_body.dart';
+import 'package:mo3tv/config/lang/app_localizations.dart';
+import 'package:mo3tv/core/utils/app_strings.dart';
+import 'package:mo3tv/core/utils/app_text_styles.dart';
+import 'package:mo3tv/features/settings/presentation/widgets/settings_screen_widgets/build_settings_screen_body.dart';
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const SettingScreenBody();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        appBar: AppBar(
+          leadingWidth: 50,
+          leading: const Icon(Icons.settings_rounded),
+          title: Text(
+            AppStrings.settings.tr(context)!,
+            style: AppTextStyles.get18BoldText(),
+          ),
+        ),
+        body: const BuildSettingsScreenBody(),
+      ),
+    );
   }
 }
