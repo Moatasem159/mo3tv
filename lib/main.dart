8b0a3c1228a7fc156a561ba3654419bbd09ae4da
moatasem <moatasemmohamed39@gmail.com>
2024-02-09 17:46:51 +0200

    [MODIFICATIONS] modify native splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:mo3tv/app/app.dart';
+import 'package:flutter/material.dart';
 import 'package:mo3tv/app/injection_container.dart'as di show init;
 void main()async{
-  FlutterNativeSplash.preserve(widgetsBinding: WidgetsFlutterBinding.ensureInitialized());
+  WidgetsFlutterBinding.ensureInitialized();
   await di.init();
   runApp(const Mo3Tv());
-  FlutterNativeSplash.remove();
 }
