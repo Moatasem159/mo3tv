76e5accf879af1e6c210926b816a1738f492853a
Moatasem mohamed <moatasemmohamed39@gmail>
2024-09-18 19:46:05 +0300

    [UPDATE] dio and end points

--- lib/core/api/end_points.dart
+++ lib/core/api/end_points.dart
@@ -1,50 +1,67 @@
-import 'package:mo3tv/core/api/env.dart';
 import 'package:mo3tv/features/media/domain/entities/media_params.dart';
-abstract class EndPoints{
-  static const String baseUrl="https://api.themoviedb.org/3";
+
+abstract class EndPoints {
+  static String approveToken(String token) =>
+      "https://www.themoviedb.org/authenticate/$token";
+  static const String baseUrl = "https://api.themoviedb.org/3";
   static const String basePosterUrl = "https://image.tmdb.org/t/p/w500";
   static const String baseBackDropsUrl = "https://image.tmdb.org/t/p/w780";
   static const String baseLogoUrl = "https://image.tmdb.org/t/p/w185";
   static const String baseHdUrl = "https://image.tmdb.org/t/p/w1280";
-  static String requestTokenPath="$baseUrl/authentication/token/new?api_key=${Env.apiKey}";
-  static String approveToken(String token)=>
-      "https://www.themoviedb.org/authenticate/$token";
-  static String sessionIdPath(String token)=>
-      "$baseUrl/authentication/session/new?api_key=${Env.apiKey}&request_token=$token";
-  static String logOutPath= "$baseUrl/authentication/session?api_key=${Env.apiKey}";
+  static String requestTokenPath = "/authentication/token/new";
+
+  static String sessionIdPath(String token) =>
+      "/authentication/session/new?request_token=$token";
+  static String logOutPath = "/authentication/session";
+
   static String mediaDetailsPath(String sessionId, MediaParams params) =>
-      "$baseUrl/${params.mediaType}/${params.mediaId}?api_key=${Env.apiKey}&session_id=$sessionId&append_to_response=keywords,account_states,videos&language=${params.lang}";
+      "/${params.mediaType}/${params.mediaId}?session_id=$sessionId&append_to_response=keywords,account_states,videos&language=${params.lang}";
+
   static String discoverPath(MediaParams params) =>
-      "$baseUrl/discover/${params.mediaType}?api_key=${Env.apiKey}&language=${params.lang}&with_genres=${params.genres}&page=${params.page}";
-  static String trendingMediaPath (MediaParams params)=>
-      "$baseUrl/trending/${params.mediaType}/day?api_key=${Env.apiKey}&page=${params.page}&language=${params.lang}";
+      "/discover/${params.mediaType}?language=${params.lang}&with_genres=${params.genres}&page=${params.page}";
+
+  static String trendingMediaPath(MediaParams params) =>
+      "/trending/${params.mediaType}/day?page=${params.page}&language=${params.lang}";
+
   static String mediaListsPath(MediaParams params) =>
-      "$baseUrl/${params.mediaType}/${params.listType}?api_key=${Env.apiKey}&page=${params.page}&language=${params.lang}";
+      "/${params.mediaType}/${params.listType}?page=${params.page}&language=${params.lang}";
+
   static String similarMediaPath(MediaParams params) =>
-      "$baseUrl/${params.mediaType}/${params.mediaId}/${params.moreType}?api_key=${Env.apiKey}&page=${params.page}&language=${params.lang}";
+      "/${params.mediaType}/${params.mediaId}/${params.moreType}?page=${params.page}&language=${params.lang}";
+
   static String mediaReviewsPath(MediaParams params) =>
-      "$baseUrl/${params.mediaType}/${params.mediaId}/reviews?api_key=${Env.apiKey}";
+      "/${params.mediaType}/${params.mediaId}/reviews";
+
   static String mediaCreditsPath(MediaParams params) =>
-      "$baseUrl/${params.mediaType}/${params.mediaId}/credits?api_key=${Env.apiKey}";
+      "/${params.mediaType}/${params.mediaId}/credits";
+
   static String mediaGalleryPath(MediaParams params) =>
-      "$baseUrl/${params.mediaType}/${params.mediaId}/images?api_key=${Env.apiKey}";
-  static String markMediaPath(String sessionid,String markType)=>
-      "$baseUrl/account/{account_id}/$markType?api_key=${Env.apiKey}&session_id=$sessionid";
-  static String rateMediaPath(String sessionid,MediaParams params)=>
-      "$baseUrl/${params.mediaType}/${params.mediaId}/rating?api_key=${Env.apiKey}&session_id=$sessionid";
+      "/${params.mediaType}/${params.mediaId}/images";
+
+  static String markMediaPath(String sessionid, String markType) =>
+      "/account/{account_id}/$markType?session_id=$sessionid";
+
+  static String rateMediaPath(String sessionid, MediaParams params) =>
+      "/${params.mediaType}/${params.mediaId}/rating?session_id=$sessionid";
+
   static String tvShowSeasonDetailsPath(MediaParams params) =>
-      "$baseUrl/tv/${params.mediaId}/season/${params.seasonNumber}?api_key=${Env.apiKey}&language=${params.lang}";
-  static String searchUrl({required String query,required int page,required String lang})=>
-      "$baseUrl/search/multi?api_key=${Env.apiKey}&page=$page&query=$query&language=$lang";
-  static String backDropsUrl(String path) =>
-      "$baseBackDropsUrl$path";
-  static String posterUrl(String path) =>
-      "$basePosterUrl$path";
-  static String logoUrl(String path) =>
-      "$baseLogoUrl$path";
-  static String hdUrl(String path) =>
-      "$baseHdUrl$path";
-  static String accountPath(String sessionId) => "$baseUrl/account?api_key=${Env.apiKey}&session_id=$sessionId";
-  static String accountMediaListPath(String sessionid,MediaParams params)=>
-     "$baseUrl/account/{account_id}/${params.listType}/${params.mediaType}?api_key=${Env.apiKey}&session_id=$sessionid&page=${params.page}&language=${params.lang}";
+      "/tv/${params.mediaId}/season/${params.seasonNumber}?language=${params.lang}";
+
+  static String searchUrl(
+          {required String query, required int page, required String lang}) =>
+      "/search/multi?page=$page&query=$query&language=$lang";
+
+  static String backDropsUrl(String path) => "$baseBackDropsUrl$path";
+
+  static String posterUrl(String path) => "$basePosterUrl$path";
+
+  static String logoUrl(String path) => "$baseLogoUrl$path";
+
+  static String hdUrl(String path) => "$baseHdUrl$path";
+
+  static String accountPath(String sessionId) =>
+      "/account?session_id=$sessionId";
+
+  static String accountMediaListPath(String sessionid, MediaParams params) =>
+      "/account/{account_id}/${params.listType}/${params.mediaType}?session_id=$sessionid&page=${params.page}&language=${params.lang}";
 }
--- lib/features/auth/presentation/widgets/login_widgets/login_button_widget.dart
+++ lib/features/auth/presentation/widgets/login_widgets/login_button_widget.dart
@@ -1,10 +1,13 @@
 part of 'login_button.dart';
+
 class _LoginButtonWidget extends StatelessWidget {
   const _LoginButtonWidget();
+
   @override
   Widget build(BuildContext context) {
     return MainButton(
-      size: const Size(240, 30),
+      size: const Size(240, 35),
+      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 6),
       onPressed: () async => await LogCubit.get(context).getToken(),
       label: AppStrings.loginToEnjoyFullExperience.tr(context)!,
     );
